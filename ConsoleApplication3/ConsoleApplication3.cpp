781876f3bf51d67c0b6077fda3fc7c3681c034d7
Maria Guseva <75927476+oreshkichikaladnie@users.noreply.github.com>
2022-04-30 14:22:10 +0300

    Update ConsoleApplication3.cpp

--- ConsoleApplication3/ConsoleApplication3.cpp
+++ ConsoleApplication3/ConsoleApplication3.cpp
@@ -6,9 +6,15 @@ int main()
     double a, b, c;
     printf("Vvedite chisla\n"); scanf("%d %d %d", &a, &b, &c);
 
-    a == 0 ? b == 0 ? printf("no solutions") :
-        printf("%d", -c / b) : (b*b - 4 * a * c) > 0 ?
-        printf("%d %d", (-b + sqrt(b*b - 4 * a * c)) / 2 * a, (-b - sqrt(b*b - 4 * a * c)) / 2 * a) : (b*b - 4 * a * c) == 0 ? printf("%d", -b / 2 * a) :
+    if (a == 0 && b == 0)
+        printf("no solutions");
+    if (a==0 && b != 0) 
+        printf("%d", -c / b);
+    if ((b*b - 4 * a * c) > 0 && a!=0)
+        printf("%d %d", (-b + sqrt(b*b - 4 * a * c)) / (2 * a), (-b - sqrt(b*b - 4 * a * c)) / (2 * a)));
+    if (a!=0 && (b*b - 4 * a * c) == 0) 
+        printf("%d", -b / 2 * a);
+    else
         printf("no solutions");
         return 0;
 }
