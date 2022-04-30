89634df5abbc4089984778ba4bbb0e61098c2fe3
Maria Guseva <75927476+oreshkichikaladnie@users.noreply.github.com>
2022-04-30 14:04:09 +0300

    Update ConsoleApplication2.cpp

--- ConsoleApplication2/ConsoleApplication2.cpp
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -1,17 +1,18 @@
 #include <stdio.h>
 
-int vivod(unsigned long int x)
+void output(unsigned long long int x)
 {
     int i;
     for (i = 1; i <= 32; i++)
     {
-        if ((1 << (i - 1) & x) != 0) printf("%d ", i);
+        if ((1 << (i - 1) & x) != 0) 
+            printf("%d ", i);
     }
     printf("\n");
 }
-unsigned long int perevod(unsigned long int x)
+unsigned long long int perevod(unsigned long long int x)
 {
-    unsigned long int i, b = 0;
+    unsigned long long int i, b = 0;
     for (i = 1; i <= 32; i++)
     {
         b += (x % 10) * (1 << (i - 1));
@@ -22,12 +23,12 @@ unsigned long int perevod(unsigned long int x)
 
 int main()
 {
-    unsigned long int a, b;
+    unsigned long long int a, b;
     printf("Input a and b\n");
     scanf("%lu%lu", &a, &b);
     a = perevod(a); b = perevod(b);
-    vivod(a); vivod(b); vivod(a | b); vivod(a & b);
+    output(a); output(b); output(a | b); output(a & b);
 
-    vivod(a - (a & b)); vivod((a | b) - (a & b));
+    output(a - (a & b)); output((a | b) - (a & b));
     return 0;
 }
