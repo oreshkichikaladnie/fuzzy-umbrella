92fd11576bed323598753cc246c9a1eb81bbafd4
Maria Guseva <75927476+oreshkichikaladnie@users.noreply.github.com>
2022-05-12 22:36:34 +0300

    Update ConsoleApplication3.cpp

--- ConsoleApplication3/ConsoleApplication3.cpp
+++ ConsoleApplication3/ConsoleApplication3.cpp
@@ -4,20 +4,20 @@
 int main()
 {
     double a, b, c;
-    printf("Vvedite chisla\n"); scanf("%d %d %d", &a, &b, &c);
+    printf("Vvedite chisla\n"); scanf("%lf %lf %lf", &a, &b, &c);
 
     if (a == 0){
         if(b==0)
             printf("no solutions\n");
         else
-            printf("the solution is %d\n",-c/b);
+            printf("the solution is %lf\n",-c/b);
     }
     else{
         double d=b*b-4*a*c;
         if (d>0)
-            printf("the solutions are %d %d\n",(-b + sqrt(d))/(2*a),(-b-sqrt(d))/(2*a));
+            printf("the solutions are %lf %lf\n",(-b + sqrt(d))/(2*a),(-b-sqrt(d))/(2*a));
         if (d==0)
-            printf("the solution is %d\n",-b/(2*a));
+            printf("the solution is %lf\n",-b/(2*a));
         else
             printf("No solutions \n");
     }
